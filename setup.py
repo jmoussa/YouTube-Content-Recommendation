69cf481db894b759278579d9728fedcf0b5f6bb6
Joseph Moussa <joeph465@gmail.com>
2021-04-14 20:19:36 -0400

    fix setup.py and update gitignore

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ from setuptools import setup, find_namespace_packages
 def get_requirements():
     basedir = os.path.dirname(__file__)
     with open(os.path.join(basedir, "requirements.txt")) as f:
-        return [l.strip() for l in f]
+        return [line.strip() for line in f]
 
 
-print(find_namespace_packages(include="aggtube"))
+print(find_namespace_packages(include=["aggtube.*"]))
 setup(
     name="aggtube",
     version="0.0.1",
-    packages=find_namespace_packages(include="aggtube."),
+    packages=find_namespace_packages(include=["aggtube.*"]),
     zip_safe=False,
     install_requires=get_requirements(),
     scripts=[],
